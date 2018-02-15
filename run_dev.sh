548b0b7880bf83338716100d1febfc0fcad1128e
gregorwebmaster <git@gregorwebmaster.com>
2018-02-15 12:38:40 +0100

    add update the root password

--- run_dev.sh
+++ run_dev.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 docker build -t maria-db .
-docker run --name maria-db -d -e DATABASE_ADMIN='superadmin' -e ADMIN_PSWD='Pizza!23' maria-db
+docker run --name maria-db -d -e ROOT_PSWD='topsecret' -e DATABASE_ADMIN='superadmin' -e ADMIN_PSWD='Pizza!23' maria-db
 docker ps -a -f name='maria-db'
 
 echo "configuration ...."
 
-sleep 15
+sleep 12
 docker logs maria-db
--- scripts/run.sh
+++ scripts/run.sh
@@ -21,23 +21,29 @@ else
     fi
 
     echo "DROP DATABASE test;" > $TMP_SQL
+    echo "FLUSH PRIVILEGES;" >> $TMP_SQL
 
+    ROOT_PSWD=${ROOT_PSWD:-""}
     DATABASE_ADMIN=${DATABASE_ADMIN:-""}
     ADMIN_PSWD=${ADMIN_PSWD:-""}
+
+    if [ "$ROOT_PSWD" != "" ]; then
+        echo "Set root password"
+        echo "UPDATE mysql.user SET password=PASSWORD('$ROOT_PSWD') WHERE User='root';" >> $TMP_SQL
+    fi
 
     if [ "$DATABASE_ADMIN" != "" ] && [ "$ADMIN_PSWD" != "" ]; then
         echo "Create admin user"
 
-        echo "USE mysql;" >> $TMP_SQL
-        echo "FLUSH PRIVILEGES;" >> $TMP_SQL
 	    echo "GRANT ALL ON *.* to '$DATABASE_ADMIN'@'%' IDENTIFIED BY '$ADMIN_PSWD';" >> $TMP_SQL
+	    echo "GRANT ALL ON *.* to '$DATABASE_ADMIN'@'localhost' IDENTIFIED BY '$ADMIN_PSWD';" >> $TMP_SQL
 	    echo "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = '$DATABASE_ADMIN';" >> $TMP_SQL
 	    echo "FLUSH PRIVILEGES;" >> $TMP_SQL
 
     fi
+        echo "Update configuration"
+	    /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $TMP_SQL
+        rm -f $TMP_SQL
 fi
-    echo "Update configuration"
-	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 < $TMP_SQL
-    rm -f $TMP_SQL
 
 exec /usr/bin/mysqld --user=mysql --console
